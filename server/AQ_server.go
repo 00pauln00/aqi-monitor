9cf8639f71ee1f40178d4b94768bd311310970d5
Meerthika <meerthikasrrajendrans@gmail.com>
2025-09-08 21:49:36 +0530

    [Remove] comment on import C

--- server/AQ_server.go
+++ server/AQ_server.go
@@ -11,15 +11,15 @@ import (
 	AQLib "github.com/00pauln00/aqi-monitor/lib"
 	PumiceDBServer "github.com/00pauln00/niova-pumicedb/go/pkg/pumiceserver"
 	log "github.com/sirupsen/logrus"
-	PumiceDBCommon "github.com/00pauln00/niova-pumicedb/go/pkg/pumicecommon"
-    PumiceDBFunc "github.com/00pauln00/niova-pumicedb/go/pkg/pumicefunc/server"
+	// PumiceDBCommon "github.com/00pauln00/niova-pumicedb/go/pkg/pumicecommon"
+    // PumiceDBFunc "github.com/00pauln00/niova-pumicedb/go/pkg/pumicefunc/server"
 )
 
 /*
 #include <stdlib.h>
 #include <string.h>
 */
-// import "C"
+import "C"
 
 var seqno = 0
 var raftUuid string
